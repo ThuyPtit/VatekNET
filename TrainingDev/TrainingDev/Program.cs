22e063c82e9fa44446a68b3a464aa26a41c2e657
nam.nguyen <nam.nguyen@websynergies.com>
2022-08-31 17:56:57 +0700

    add new project

---
+++ TrainingDev/TrainingDev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrainingDev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
